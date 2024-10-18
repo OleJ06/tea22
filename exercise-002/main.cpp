26486a532facf55c5bc83168414db9cba4a68aa3
OleJ06 <126180069+OleJ06@users.noreply.github.com>
2024-10-18 16:14:14 +0200

    Vorlesung 18.10.24

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -1,8 +1,39 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
 
+/* .bss Segment */
+static int foo;                                         // Variablendeklaration // static nur innerhalb des Übersetzungsmoduls
+int foo2 = 0;
 
-int foo;
+/* .data Segment */
+int data = 45;
+int data2 = 43;
+
+/* .rodata Segment */
+const int rodata = 45;
+const int rodata2 = 43;
+
+void doSomething (int value) {                          // call by value (Gültigkeit nur innerhalb der Funktion)
+    value = value + 1;
+    fmt::print("doSomething, value {}\n", value);
+}
+
+void doSomethingPtr (int* ptr) {                          
+    *ptr = *ptr + 1;
+    fmt::print("doSomethingPtr, value {}\n", *ptr);
+}
+
+int* doReturnAPtr () {                          
+    static int value = 4711;                                   
+    fmt::print("doReturnAPtr, value {}\n", value);
+    return &value;                                      // das hier vermeiden (kein Zeiger auf eine Funktionslokale Variable zurückliefern) 
+}
+
+void doSomethingStatic () {
+    static int value = 56;
+    value = value+1;
+    fmt::print("doSomethingStatic, value {}\n", value);
+}
 
 auto main(int argc, char **argv) -> int
 {
@@ -15,7 +46,37 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello, {}!\n", argv[0]);
     fmt::print("Value of foo {} address of foo {}\n", foo, fmt::ptr(&foo));
 
+    doSomething(42);
+    int bar = 42;
+    doSomething(bar);
+    fmt::print("The value of bar is: {}\n", bar);
+    
+    doSomethingPtr(&bar);
+    fmt::print("The value of bar after doSomethingPtr is: {}\n", bar);
+
+    int* returnedPtr = doReturnAPtr();
+    fmt::print("The value of bar after returnedPtr content after doReturnAPtr is: {}\n", *returnedPtr);
+    doSomething(42);
+    fmt::print("The value of bar after returnedPtr content after doReturnAPtr is: {}\n", *returnedPtr);
+
+doSomethingStatic ();
+doSomethingStatic ();
+doSomethingStatic ();
+doSomethingStatic ();
 
 
-    return 0; /* exit gracefully*/
+    
+fmt::print("Value of foo     {} address of foo {}\n", foo, fmt::ptr(&foo));         // Die Adresse ist random 
+fmt::print("Value of foo2    {} address of foo2 {}\n", foo2, fmt::ptr(&foo2));
+fmt::print("Value of data    {} address of data {}\n", data, fmt::ptr(&data));         
+fmt::print("Value of data2   {} address of data2 {}\n", data2, fmt::ptr(&data2));
+fmt::print("Value of rodata  {} address of rodata {}\n", rodata, fmt::ptr(&rodata));         
+fmt::print("Value of rodata2 {} address of rodata2 {}\n", rodata2, fmt::ptr(&rodata2));
+
+int var;
+static int var2;
+fmt::print("Value of var   {} address of var {}\n", var, fmt::ptr(&var)); 
+fmt::print("Value of var2  {} address of var2 {}\n", var2, fmt::ptr(&var2)); 
+
+    return 0; /* exit gracefully*/                      // Klausur: Was ist hier die Ausgabe?
 }
